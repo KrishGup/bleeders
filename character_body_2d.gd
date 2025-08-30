8fa53b18a8965cf65c14fac965922ea8d42b837c
Trexdog319 <nbusedu@gmail.com>
2025-08-30 18:33:48 -0400

    Added Background, Character, and World Borders

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
+# Get reference to AnimatedSprite2D node
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(_delta: float) -> void:
+	# Top-down movement: get both horizontal and vertical input
+	var input_vector = Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	)
+	if input_vector.length() > 0:
+		input_vector = input_vector.normalized()
+		velocity = input_vector * SPEED
+		if not animated_sprite.is_playing():
+			animated_sprite.play()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		animated_sprite.stop()
 	move_and_slide()
